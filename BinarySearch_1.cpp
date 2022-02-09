f9858d592a218200d8116afc29dc194f5f7ed05d
Amitesh Bharti <amitesharyan774@gmail.com>
2022-02-09 11:43:50 +0530

    Create BinarySearch_1.cpp

---
+++ BinarySearch_1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstring>
+#include <math.h>
+using namespace std;
+
+int binarySearch(int arr[], int size, int n) {
+    
+    int low = 0;
+    int high = size-1;
+    int mid;
+    for(int i = 0; i < size; i++) {
+        mid = floor(low + ((high - low) / 2));
+        
+        if(n == arr[mid]) {
+            return mid;
+        }
+        
+        if(n > arr[mid]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+
+int main(int argc, const char * argv[]) {
+    int arr[] = {10, 20, 30, 41, 40, 50};
+    int size = sizeof(arr)/sizeof(int);
+    int n;
+    cout << "Enter the number you want to search?" << endl;
+    cin >> n;
+    
+    int output = binarySearch(arr, size, n);
+    
+    if (output == -1) {
+        cout << "Number Not Found";
+    } else {
+        cout << "Number found at index no " << output;
+    }
+    
+    cout <<endl;
+    
+ //   cout << output;
+    
+    return 0;
+}
